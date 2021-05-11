08f2818bc9465429099e77b87b132ab7f12b6980
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2021-05-10 23:39:48 -0300

    projeto em esmodule

---
+++ 01-esmodules/esmodule/node/index.mjs
@@ -0,0 +1,11 @@
+import CurrencyManager from "shared/currencyManager"
+
+function updateOutput(value) {
+    console.log(value)
+}
+
+const inputMonitor = (value) => updateOutput(CurrencyManager.format(value))
+
+const input = process.openStdin()
+console.log('typing...')
+input.addListener("data", inputMonitor)
